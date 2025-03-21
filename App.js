a0d5ef1e980dccba57e8d6a3537b936446e250c1
Aman Jain <118008851+jainaman78@users.noreply.github.com>
2025-03-21 20:22:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import SignIn from "./SignIn";
+
+const products = [
+  {
+    id: 1,
+    name: "Men's Trendy Shorts",
+    image: "https://tse2.mm.bing.net/th?id=OIP.ceetrT3VLc6d60mPj981NgHaHk&pid=Api&P=0&h=180",
+    link: "https://www.meesho.com/your-product-link",
+  },
+  {
+    id: 2,
+    name: "Ladies' Elegant Kurti",
+    image: "https://tse2.mm.bing.net/th?id=OIP.ceetrT3VLc6d60mPj981NgHaHk&pid=Api&P=0&h=180",
+    link: "https://www.meesho.com/your-product-link",
+  },
+];
+
+
+
+function App() {
+  return (
+    <SignIn></SignIn>
+  );
+}
+
+export default App;
